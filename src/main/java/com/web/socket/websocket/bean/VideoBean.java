304dd29300f87e6741c0e29b7ed80f319c026cce
Vaideeswaran C <vaideeswaran@agentz.ai>
2020-12-02 10:10:51 +0530

    Added video support

---
+++ src/main/java/com/web/socket/websocket/bean/VideoBean.java
@@ -0,0 +1,23 @@
+package com.web.socket.websocket.bean;
+
+public class VideoBean {
+
+    private String sdp;
+    private String type;
+
+    public String getSdp() {
+        return sdp;
+    }
+
+    public void setSdp(String sdp) {
+        this.sdp = sdp;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
--- src/main/java/com/web/socket/websocket/controller/SocketController.java
+++ src/main/java/com/web/socket/websocket/controller/SocketController.java
@@ -1,6 +1,7 @@
 package com.web.socket.websocket.controller;
 
 import com.web.socket.websocket.bean.MessageBean;
+import com.web.socket.websocket.bean.VideoBean;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -12,6 +13,18 @@ public class SocketController {
     @MessageMapping("/user-all")
     @SendTo("/topic/user")
     public MessageBean send(@Payload MessageBean message) {
+        return message;
+    }
+
+    @MessageMapping("/video-all")
+    @SendTo("/topic/video")
+    public VideoBean video(@Payload VideoBean message) {
+        return message;
+    }
+
+    @MessageMapping("/answer-all")
+    @SendTo("/topic/answer")
+    public VideoBean answer(@Payload VideoBean message) {
         return message;
     }
 
